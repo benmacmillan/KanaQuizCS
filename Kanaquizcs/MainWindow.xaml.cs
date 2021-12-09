94319d209555658a48bf58ac5660e931f5055cc0
Ben <45116438+benmacmillan@users.noreply.github.com>
2021-12-09 20:17:32 +0800

    added small dict (aiueo) to test random questions

--- Kanaquizcs/MainWindow.xaml.cs
+++ Kanaquizcs/MainWindow.xaml.cs
@@ -26,10 +26,35 @@ namespace Kanaquizcs
         public string Chosen_Answer = "a";
         public int Correct = 0;
         public int Incorrect = 0;
+        Random randomInstance = new Random();
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            NewQuestion();
         }
+
+        private void NewQuestion()
+        {
+            var quizdict = new Dictionary<string, string>()
+        {
+            {"あ","a" },
+            {"い","i" },
+            {"う","u" },
+            {"え","e" },
+            {"お","o" }
+        };
+            int index = randomInstance.Next(quizdict.Count);
+            KeyValuePair<string, string> pair = quizdict.ElementAt(index);
+            Chosen_Answer = pair.Value ; 
+            Question_FG.Text = pair.Key;
+            Question_BG.Text = pair.Key;
+
+        }
+
         private void Exit_Mousedown(object sender, MouseButtonEventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
@@ -45,15 +70,16 @@ namespace Kanaquizcs
                 Answer_Response.Text = "Correct!";
                 Answer_Response.Foreground = new SolidColorBrush(Color.FromRgb(46, 204, 113));
                 Question_FG.Foreground = new SolidColorBrush(Color.FromRgb(46, 204, 113));
-                Correct = +1;
+                Correct += 1;
                 Score.Text = ("R: " + Correct + " W: " + Incorrect);
+                NewQuestion();
             }
             else
             {
                 Answer_Response.Text = "Incorrect!";
                 Answer_Response.Foreground = new SolidColorBrush(Color.FromRgb(231, 76, 60));
                 Question_FG.Foreground = new SolidColorBrush(Color.FromRgb(231, 76, 60));
-                Incorrect = +1;
+                Incorrect += 1;
                 Score.Text = ("R: " + Correct + " W: " + Incorrect); 
             }
         }
