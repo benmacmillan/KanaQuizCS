7b287bdba963109a3fe034867b119198c9d0efcb
Ben <45116438+benmacmillan@users.noreply.github.com>
2021-12-10 16:30:00 +0800

    Added katakana dict, unsure if these will be merged or toggled, added toggle for enabling/disabling hiragana/katakana

--- Kanaquizcs/MainWindow.xaml.cs
+++ Kanaquizcs/MainWindow.xaml.cs
@@ -26,6 +26,8 @@ namespace Kanaquizcs
         public string Chosen_Answer = "a";
         public int Correct = 0;
         public int Incorrect = 0;
+        public bool EnableHiragana = true;
+        public bool EnableKatakana = false;
         Random randomInstance = new Random();
 
 
@@ -39,7 +41,7 @@ namespace Kanaquizcs
 
         private void NewQuestion()
         {
-            var quizdict = new Dictionary<string, string>()
+            var HiraganaDict = new Dictionary<string, string>()
         {
             {"あ","a" },{"い","i" },{"う","u" },{"え","e" },{"お","o" },
             {"か","ka" },{"き","ki" },{"く","ku" },{"け","ke" },{"こ","ko" },{"きゃ","kya" },{"きゅ","kyu" },{"きょ","kyo" },
@@ -50,13 +52,35 @@ namespace Kanaquizcs
             {"ま","ma" },{"み","mi" },{"む","mu" },{"め","me" },{"も","mo" },{"みゃ","mya" },{"みゅ","myu" },{"みょ","myo" },
             {"や","ya" },{"ゆ","yu" },{"よ","yo" },
             {"ら","ra" },{"り","ri" },{"る","ru" },{"れ","re" },{"ろ","ro" },{"りゃ","rya" },{"りゅ","ryu" },{"りょ","ryo" },
-            {"わ","wa" },{"を","wo" }
-
-
-
+            {"わ","wa" },{"を","wo" },
+            {"が","ga" },{"ぎ","gi" },{"ぐ","gu" },{"げ","ge" },{"ご","go" },{"ぎゃ","gya" },{"ぎゅ","gyu" },{"ぎょ","gyo" },
+            {"ざ","za" },{"じ","ji" },{"ず","zu" },{"ぜ","ze" },{"ぞ","zo" },{"じゃ","ja" },{"じゅ","ju" },{"じょ","jo" },
+            {"だ","da" },{"ぢ","zi" },{"づ","zu" },{"で","de" },{"ど","do" },
+            {"ば","ba" },{"び","bi" },{"ぶ","bu" },{"べ","be" },{"ぼ","bo" },{"びゃ","bya" },{"びゅ","byu" },{"びょ","byo" },
+            {"ぱ","pa" },{"ぴ","pi" },{"ぷ","pu" },{"ぺ","pe" },{"ぽ","po" },{"ぴゃ","pya" },{"ぴゅ","pyu" },{"ぴょ","pyo" }
         };
-            int index = randomInstance.Next(quizdict.Count);
-            KeyValuePair<string, string> pair = quizdict.ElementAt(index);
+
+            var KatakanaDict = new Dictionary<string, string>()
+        {
+            {"ア","a" },{"イ","i" },{"ウ","u" },{"エ","e" },{"オ","o" },
+            {"カ","ka" },{"キ","ki" },{"ク","ku" },{"ケ","ke" },{"コ","ko" },{"キャ","kya" },{"キュ","kyu" },{"キョ","kyo" },
+            {"サ","sa" },{"シ","shi" },{"ス","su" },{"セ","se" },{"ソ","so" },{"シャ","sha" },{"シュ","shu" },{"ショ","sho" },
+            {"タ","ta" },{"チ","chi" },{"ツ","tsu" },{"テ","te"},{"ト","to" },{"チャ","cha" },{"チュ","chu" },{"チョ","cho" },
+            {"ナ","na" },{"ニ","ni" },{"ヌ","nu" },{"ネ","ne" },{"ノ","no" },{"ニャ","nya" },{"ニュ","nyu" },{"ニョ","nyo" },
+            {"ハ","ha" },{"ヒ","hi" },{"フ","fu" },{"ヘ","he" },{"ホ","ho" },{"ヒャ","hya" },{"ヒュ","hyu" },{"ヒョ","hyo" },
+            {"マ","ma" },{"ミ","mi" },{"ム","mu" },{"メ","me" },{"モ","mo" },{"ミャ","mya" },{"ミュ","myu" },{"ミョ","myo" },
+            {"ヤ","ya" },{"ユ","yu" },{"ヨ","yo" },
+            {"ラ","ra" },{"リ","ri" },{"ル","ru" },{"レ","re" },{"ロ","ro" },{"リャ","rya" },{"リュ","ryu" },{"リョ","ryo" },
+            {"ワ","wa" },{"ヲ","wo" },
+            {"ガ","ga" },{"ギ","gi" },{"グ","gu" },{"ゲ","ge" },{"ゴ","go" },{"ギャ","gya" },{"ギュ","gyu" },{"ギョ","gyo" },
+            {"ザ","za" },{"ジ","ji" },{"ズ","zu" },{"ゼ","ze" },{"ゾ","zo" },{"ジャ","ja" },{"ジュ","ju" },{"ジョ","jo" },
+            {"ダ","da" },{"ヂ","ji" },{"ヅ","zu" },{"デ","de" },{"ド","do" },
+            {"バ","ba" },{"ビ","bi" },{"ブ","bu" },{"ベ","be" },{"ボ","bo" },{"ビャ","bya" },{"ビュ","byu" },{"ビョ","byo" },
+            {"パ","pa" },{"ピ","pi" },{"プ","pu" },{"ペ","pe" },{"ポ","po" },{"ピャ","pya" },{"ピュ","pyu" },{"ピョ","pyo" }
+        };
+
+            int index = randomInstance.Next(HiraganaDict.Count);
+            KeyValuePair<string, string> pair = HiraganaDict.ElementAt(index);
             Chosen_Answer = pair.Value ; 
             Question_FG.Text = pair.Key;
             Question_BG.Text = pair.Key;
@@ -99,6 +123,42 @@ namespace Kanaquizcs
                     CheckAnswer();
                     answer.Clear();
             }
+        }
+
+        private void Hiragana_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            if (EnableHiragana == true & EnableKatakana == true)
+            {
+            HiraganaToggleText.Foreground = new SolidColorBrush(Color.FromRgb(52, 73, 94));
+            EnableHiragana = false;
+            }
+
+            else
+            {
+            HiraganaToggleText.Foreground = new SolidColorBrush(Color.FromRgb(46, 204, 113));
+            EnableHiragana = true;
+            }
+           
+        }
+
+        private void Katakana_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            if (EnableKatakana == true & EnableHiragana == true)
+            {
+                KatakanaToggleText.Foreground = new SolidColorBrush(Color.FromRgb(52, 73, 94));
+                EnableKatakana = false;
+            }
+
+            else
+            {
+                KatakanaToggleText.Foreground = new SolidColorBrush(Color.FromRgb(46, 204, 113));
+                EnableKatakana = true;
+            }
+        }
+
+        private void TextBlock_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+
         }
     }
 }
